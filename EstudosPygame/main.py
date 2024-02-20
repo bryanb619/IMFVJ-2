562a76a67c6a9df9353b7ae6ec50f595ac94acd0
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-02-20 12:04:04 +0000

    Correction main.py and new file Game.py

--- EstudosPygame/main.py
+++ EstudosPygame/main.py
@@ -2,24 +2,26 @@ import pygame
 
 pygame.init()
 
-
+# screen size variables
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
 run = True
 
+# Game loop
 while run:
     
-    
+    # Loop for handling game exit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
-            
+         
+    # updates screen   
+    pygame.display.update()
+    
+# Quit method is out of the loop since run = False   
 pygame.quit()
     
 
