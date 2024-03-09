f573583a359ae86c618729543d5bc981c7150c74
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-09 15:31:35 +0000

    Added correct dynamic mov

--- Aula03/ex5_Dynamic.py
+++ Aula03/ex5_Dynamic.py
@@ -1,7 +1,20 @@
 import pygame
+import numpy as np
+
+
+pos = [100, 100]
+target = [800, 100]
+
+velocity = 1
+
+slowRadius = 300
+
+_maxVelocity = 1.5
 
 
 def main():
+    global pos, target, velocity, slowRadius, _maxVelocity
+    
     pygame.init()
 
     # screen size variables
@@ -10,17 +23,13 @@ def main():
 
     # Screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    # screen
-    screen.fill((0,0,0))
-    
-    
-    p = p_ + v_t + (1/2) * a_t
-    
     run = True
 
     # Game loop
     while run:
+        
+        # screen
+        screen.fill((0,0,0))
         
         # Loop for handling game exit
         for event in pygame.event.get():
@@ -28,8 +37,41 @@ def main():
                 run = False
                 
                 
+            
+        # obter vetor direção
+        v_dir = np.subtract(target,pos) 
+        
+        # normalizar vetor direção e aplicar em distancia
+        distance = np.linalg.norm(v_dir)
+        
+       # print(v_dir)
+                
+        if (distance < slowRadius):
+            
+            # APLICAR A VELOCIDADE REDUZIDA
+            v_dir_norm = v_dir/np.linalg.norm(v_dir) * _maxVelocity * (distance/slowRadius)
+            v_dir_norm = v_dir_norm * velocity
+            pos = np.add(pos, v_dir_norm)
 
             
+        else:
+                    
+            # NORMALIZAÇÃO NORMAL
+            v_dir_norm = v_dir/np.linalg.norm(v_dir)
+            
+            v_dir_norm = v_dir_norm * velocity
+        
+            pos = np.add(pos, v_dir_norm)
+        
+            
+            
+            
+            
+        pygame.draw.circle(screen, (255,0,0), (pos), 10)
+        
+        pygame.draw.circle(screen, (0,255,0), (target), 12)
+        
+    
         # updates screen   
         pygame.display.update()
         
